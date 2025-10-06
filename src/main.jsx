fcf569b38df8e37ab10b07309b234f8ab56028b0
Kishore270426 <kishorek2704@gmail.com>
2025-10-06 11:32:14 +0530

    commit updated google client

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId='800853033309-950kriq89s5npjck3tpg0qppk0lue4h3.apps.googleusercontent.com'>
+    <GoogleOAuthProvider clientId='260241037443-irotoh5dl3hco11ef44r26gnlsmhsu4q.apps.googleusercontent.com'>
       {/* <UserProvider> */}
       <App />
       {/* </UserProvider> */}
