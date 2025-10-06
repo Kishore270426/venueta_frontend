b40e70982bf811bf2798b066dfcb77057e0edef5
Kishore270426 <kishorek2704@gmail.com>
2025-10-06 14:57:59 +0530

    updated url

--- src/pages/config.jsx
+++ src/pages/config.jsx
@@ -1,5 +1,5 @@
 // const BASE_URL = 'http://192.168.1.7:8000';
-const BASE_URL = 'https://venueta.vpearlsolutions.com/backend';
+const BASE_URL = 'https://venueta.vpearlsolutions.com/backend/';
 // const BASE_URL = 'http://34.46.33.65:8080';
 
 // for live production
