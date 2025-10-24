5824008662003f831b23329a79bd9bbe30ca9e28
Kishore270426 <140709288+Kishore270426@users.noreply.github.com>
2025-10-24 16:35:13 +0530

    Update secrets.jsx

--- src/secrets.jsx
+++ src/secrets.jsx
@@ -1,4 +1,4 @@
 // const clientId="800853033309-p60s6rqibcs03ntfs1belsjsnov1jh2j.apps.googleusercontent.com";
-const clientId="1003591627213-ad69nv6jaehu5epv4shc5a7oa1p47r55.apps.googleusercontent.com"
+const clientId="1003591627213-ld2uhndumgh1ng58kmhmaa2fqdkdo8eo.apps.googleusercontent.com"
 
 export default clientId
