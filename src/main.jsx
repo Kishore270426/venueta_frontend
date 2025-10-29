2452c67a250c2353528336c5cd7452e2d40adc38
Kishore270426 <kishorek2704@gmail.com>
2025-10-29 12:24:11 +0530

    updated src

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx'
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import clientId from './secrets.jsx';
 
-//const clientIds = "800853033309-p60s6rqibcs03ntfs1belsjsnov1jh2j.apps.googleusercontent.com";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
